56afce53e401679d0fa02776e1ddf53c07b07005
Jose Mangandid <skizofrenks@gmail.com>
2023-11-26 11:07:47 -0600

    added app-ads.txt

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,8 +3,8 @@ const router = Router();
 const fs = require('fs');
 const path = require('path');
 
-router.get('/', async (req, res)=> {
-    res.json("Hacked");
+router.get('/', async (req, res) => {
+  res.json("Hacked");
 });
 
 router.get('/app', (req, res) => {
@@ -14,27 +14,67 @@ router.get('/app', (req, res) => {
 });
 
 router.get('/:nombreArchivo', (req, res) => {
-    const nombreArchivo = req.params.nombreArchivo;
-    const rutaArchivo = path.join(__dirname, '..', 'app-json', nombreArchivo);
-  
+
+  const regex = /\.(html|json|txt)$/i;
+  const regexJSON = /\.(json)$/i;
+  const regexTXT = /\.(txt)$/i;
+  const regexHTML = /\.(HTML)$/i;
+  const nombreArchivo = req.params.nombreArchivo;
+
+
+  if(regex.test(nombreArchivo)){
+    if(regexJSON.test(nombreArchivo)){
+      readJSONFile(nombreArchivo, res)
+    } else if(regexHTML.test(nombreArchivo)){
+      readHTMLFile(nombreArchivo, res)
+    } else if(regexTXT.test(nombreArchivo)){
+      readTXTFile(nombreArchivo, res)
+    }
+  } else {
+    return res.status(500).json({ error: 'Ruta no válida' });
+  }
+
+
+});
+
+async function readJSONFile(fileName, res){
+  const rutaArchivo = path.join(__dirname, '..', 'public_files', fileName);
     fs.readFile(rutaArchivo, 'utf8', (err, data) => {
       if (err) {
-        return res.status(500).json({ error: 'No se pudo leer el archivo JSON.' });
+        return res.status(500).json({ error: 'No se pudo leer el archivo.' });
       }
-  
+
       try {
         const jsonData = JSON.parse(data);
         res.json(jsonData);
       } catch (parseError) {
-        if(nombreArchivo.includes("html")){
-          res.send(data);
-        } else {
-          res.status(500).json({ error: 'No se pudo leer el archivo JSON.' });
-        }
+        res.status(500).json({ error: 'No se pudo leer el archivo JSON.' });
       }
     });
-  });
+}
 
-  
+async function readHTMLFile(fileName, res){
+  const rutaArchivo = path.join(__dirname, '..', 'public_files', fileName);
+    fs.readFile(rutaArchivo, 'utf8', (err, data) => {
+      if (err) {
+        return res.status(500).json({ error: 'No se pudo leer el archivo HTML.' });
+      }
+
+      res.send(data);
+    });
+}
+
+async function readTXTFile(fileName, res){
+  const rutaArchivo = path.join(__dirname, '..', 'public_files', fileName);
+    fs.readFile(rutaArchivo, 'utf8', (err, data) => {
+      if (err) {
+        return res.status(500).json({ error: 'No se pudo leer el archivo TXT.' });
+      }
+
+      res.send(data);
+    });
+}
+
+
 
 module.exports = router;
